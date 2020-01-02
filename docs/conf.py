3739245decd93de45715c472b289c2aa4e0fecb4
Lili Blumenberg <lili.blumenberg@gmail.com>
2020-01-02 09:31:35 -0500

    trying to fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
+# import os
 import sys
-sys.path.insert(0, os.path.abspath('../.'))
+sys.path.insert(0, '../.')
 
 
 # -- Project information -----------------------------------------------------
@@ -22,10 +22,10 @@ copyright = '2019, Ruggleslab'
 author = 'Ruggleslab'
 
 # The short X.Y version
-version = '0.0.1'
+version = '0.0.2'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.2'
 
 
 # -- General configuration ---------------------------------------------------
--- docs/index.rst
+++ docs/index.rst
@@ -13,24 +13,6 @@ Welcome to Hypercluster's documentation!
    hypercluster
 
 
-Clustering
-================================
-.. automodule:: clustering
-  :members:
-
-
-Metrics
-================================
-.. automodule:: metrics
-  :members:
-
-
-Visualization
-================================
-.. automodule:: visualize
-  :members:
-
-
 Indices and tables
 ==================
 
