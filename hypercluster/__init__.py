b85830a67ffc317643713fcfa83a1d9621c3c9d1
Lili Blumenberg <lili.blumenberg@gmail.com>
2020-01-21 13:55:37 -0500

    bumping, changing init imports

--- hypercluster/__init__.py
+++ hypercluster/__init__.py
@@ -3,9 +3,8 @@ from hypercluster import (
     utilities, additional_clusterers, additional_metrics, classes, constants, visualize
 )
 from hypercluster.classes import AutoClusterer, MultiAutoClusterer
-__version__ = '0.1.6'
+__version__ = '0.1.7'
 __all__ = [
     "AutoClusterer",
     "MultiAutoClusterer"
 ]
-
