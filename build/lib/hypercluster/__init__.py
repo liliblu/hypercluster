67abfe2fa86bdccbdaa290ce9671e44f49d79da5
Lili Blumenberg <lili.blumenberg@gmail.com>
2020-01-21 13:54:25 -0500

    version again

--- build/lib/hypercluster/__init__.py
+++ build/lib/hypercluster/__init__.py
@@ -1,7 +1,6 @@
 import hypercluster
-from hypercluster import *
 from hypercluster.classes import AutoClusterer, MultiAutoClusterer
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 __all__ = [
     "AutoClusterer",
     "MultiAutoClusterer"
--- hypercluster/__init__.py
+++ hypercluster/__init__.py
@@ -1,4 +1,7 @@
 import hypercluster
+from hypercluster import (
+    utilities, additional_clusterers, additional_metrics, classes, constants, visualize
+)
 from hypercluster.classes import AutoClusterer, MultiAutoClusterer
 __version__ = '0.1.6'
 __all__ = [
