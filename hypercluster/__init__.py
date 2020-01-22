860309b4d45d0bb88f6765cdb5889f94ff502d1b
Lili Blumenberg <lili.blumenberg@gmail.com>
2020-01-21 21:57:16 -0500

    Bump version: 0.1.7 → 0.1.8

--- hypercluster/__init__.py
+++ hypercluster/__init__.py
@@ -5,7 +5,7 @@ from hypercluster import (
     utilities, additional_clusterers, additional_metrics, classes, constants, visualize
 )
 from hypercluster.classes import AutoClusterer, MultiAutoClusterer
-__version__ = '0.1.7'
+__version__ = '0.1.8'
 __all__ = [
     "AutoClusterer",
     "MultiAutoClusterer"
