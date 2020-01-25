fc4658cbca98a4b7eaf419741b889eacda249d5d
Lili Blumenberg <lili.blumenberg@gmail.com>
2020-01-25 10:53:24 -0500

    bumping and fixing reqs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 files = ['snakemake/hypercluster.smk', 'snakemake/config.yml']
 setuptools.setup(
     name="hypercluster",
-    version="0.1.10",
+    version="0.1.11",
     author="Lili Blumenberg, Ruggles Lab",
     author_email="lili.blumenberg@gmail.com",
     description="A package for automatic clustering hyperparameter optmization",
@@ -27,8 +27,9 @@ setuptools.setup(
         "scikit-learn >= 0.22.0",
         "hdbscan >= 0.8.24",
         "snakemake >= 5.8.2",
-        "networkx >= 2.4",
-        "python-louvain >= 0.13"
+        "igraph >=0.7.1",
+        "leidenalg >=0.7.0",
+        "louvain >=0.6.1"
     ],
     package_data={"hypercluster": files},
     packages=setuptools.find_packages()
